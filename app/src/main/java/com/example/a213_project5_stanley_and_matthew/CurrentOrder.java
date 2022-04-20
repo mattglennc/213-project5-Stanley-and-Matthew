80bec5f809428624ca6fc687975db4e1c64a8075
mattglennc <82553731+mattglennc@users.noreply.github.com>
2022-04-20 13:34:26 -0400

    All layouts done and launcher icon added.

--- app/src/main/java/com/example/a213_project5_stanley_and_matthew/CurrentOrder.java
+++ app/src/main/java/com/example/a213_project5_stanley_and_matthew/CurrentOrder.java
@@ -1,15 +1,28 @@
 package com.example.a213_project5_stanley_and_matthew;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CurrentOrder extends AppCompatActivity {
 
+    private Button placeOrder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        placeOrder = findViewById(R.id.placeOrder);
+        placeOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),
+                        "Order has been placed.", Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
